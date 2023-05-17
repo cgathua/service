a3a70150471355f1ba733ba4fb852160ec529785
cgathua <114153640+cgathua@users.noreply.github.com>
2023-05-17 09:21:44 -0400

    create: media queries

--- main.js
+++ main.js
@@ -12,3 +12,18 @@ faqs.forEach((faq) => {
     });
 });
 // END OF FAQ JAVASCRIPT
+
+const menuBtn = document.querySelector('#menu-btn');
+const navMenu = document.querySelector('#navbar2');
+
+menuBtn.addEventListener('click', () =>{
+    menuBtn.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll('.nav-link').forEach(n => n.addEventListener
+    ('click', () => {
+        menuBtn.classList.remove('active');
+        navMenu.classList.remove('active');
+    }))
+// END OF HAMBURGER MENU
